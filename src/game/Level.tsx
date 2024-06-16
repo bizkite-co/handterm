ce15c1a7c7e7d7fd4a51bfd45f5771eafb9cea7a
Mark Stouffer <1802850+InTEGr8or@users.noreply.github.com>
2024-06-15 18:30:40 -0700

    Update levels

--- src/game/Level.tsx
+++ src/game/Level.tsx
@@ -3,18 +3,18 @@ import { IParallaxLayer } from './ParallaxLayer';
 
 export const layers: IParallaxLayer[][] = [
   [
-    { imageSrc: '/images/parallax-industrial-pack/parallax-industrial-pack/layers/skill-desc_0003_bg.png', scale: 0.8, movementRate: 0.0 },
+    { imageSrc: '/images/parallax-industrial-pack/parallax-industrial-pack/layers/skill-desc_0003_bg.png', scale: 1, movementRate: 0.0 },
     { imageSrc: '/images/parallax-industrial-pack/parallax-industrial-pack/layers/skill-desc_0002_far-buildings.png', scale: 0.8, movementRate: 0.4 },
     { imageSrc: '/images/parallax-industrial-pack/parallax-industrial-pack/layers/skill-desc_0001_buildings.png', scale: 0.6, movementRate: 0.6 },
     { imageSrc: '/images/parallax-industrial-pack/parallax-industrial-pack/layers/skill-desc_0000_foreground.png', scale: 0.6, movementRate: 1 },
   ],
   [
-    { imageSrc: '/images/cyberpunk-street-files/cyberpunk-street-files/PNG/layers/far-buildings.png', scale: 0.6, movementRate: 0.4 },
+    { imageSrc: '/images/cyberpunk-street-files/cyberpunk-street-files/PNG/layers/far-buildings.png', scale: 1, movementRate: 0.4 },
     { imageSrc: '/images/cyberpunk-street-files/cyberpunk-street-files/PNG/layers/back-buildings.png', scale: 0.8, movementRate: 0.6 },
     { imageSrc: '/images/cyberpunk-street-files/cyberpunk-street-files/PNG/layers/foreground.png', scale: 1, movementRate: 1 },
   ],
   [
-    { imageSrc: '/images/post_soviet_city_parallax/post_soviet_city_parallax/layer_1_ground.png', scale: 0.8, movementRate: 1 },
+    { imageSrc: '/images/post_soviet_city_parallax/post_soviet_city_parallax/layer_1_ground.png', scale: 1, movementRate: 1 },
     { imageSrc: '/images/post_soviet_city_parallax/post_soviet_city_parallax/layer_2_stars.png', scale: 0.8, movementRate: 0.0 },
     { imageSrc: '/images/post_soviet_city_parallax/post_soviet_city_parallax/layer_3_moon.png', scale: 0.8, movementRate: 0.0 },
     { imageSrc: '/images/post_soviet_city_parallax/post_soviet_city_parallax/layer_4_clouds_1.png', scale: 0.8, movementRate: 0.2 },
