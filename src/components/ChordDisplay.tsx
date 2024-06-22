461e0704dc88f724623678a83fee5acb36308dc4
Mark Stouffer <1802850+InTEGr8or@users.noreply.github.com>
2024-06-22 12:08:38 -0700

    Shrink chord svg

--- src/components/ChordDisplay.tsx
+++ src/components/ChordDisplay.tsx
@@ -11,7 +11,7 @@ export function ChordDisplay(
         <div className="chord-image-holder" id="chord-image-holder" data-source="ErrorDisplay.tsx">
             <div className="col-sm-2 row generated next" id="chord2" >
                 <span id="char15" className="char">{props.displayChar}</span>
-                <img loading="lazy" alt="2" src={`${import.meta.env.BASE_URL}images/svgs/${props.displayCharCode}.svg`} width="100" className="hand"></img>
+                <img loading="lazy" alt="2" src={`${import.meta.env.BASE_URL}images/svgs/${props.displayCharCode}.svg`} width="75" className="hand"></img>
             </div>
         </div>
     )
