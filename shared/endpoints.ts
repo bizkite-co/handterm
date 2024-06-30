fa10562af7bd80709852bf05df1fab0e2cbcee57
Mark Stouffer <1802850+InTEGr8or@users.noreply.github.com>
2024-06-30 11:55:11 -0700

    Add shared/endpoints.ts

---
+++ shared/endpoints.ts
@@ -0,0 +1,26 @@
+// shared/endpoints.ts
+
+export const ENDPOINTS = {
+    aws: {
+        cognito: {
+            url: 'https://cognito-idp.us-east-1.amazonaws.com',
+        },
+        s3: {
+            url: 'https://s3.amazonaws.com',
+            bucketName: 'handterm-history',
+        },
+    },
+    api: {
+        SignUp: '/signUp',
+        GetLog: '/getLog',
+        SaveLog: '/saveLog',
+        SignIn: '/signIn',
+        SignOut: '/signOut',
+        ChangePassword: '/changePassword',
+        TokenHandler: '/tokenHandler',
+        RefreshSession: '/refreshSession',
+        CheckSession: '/checkSession',
+        GetUser: '/getUser',
+    }
+
+} as const
