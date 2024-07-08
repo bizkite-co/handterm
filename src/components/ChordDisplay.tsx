f635b130c687b9fb595c5341f87777e7581113e4
Mark Stouffer <1802850+InTEGr8or@users.noreply.github.com>
2024-07-08 10:15:37 -0700

    Fix nav SVG text-replace

--- src/components/ChordDisplay.tsx
+++ src/components/ChordDisplay.tsx
@@ -8,9 +8,6 @@ export function ChordDisplay(
     props: ChordDisplayProps
 ) {
     // TODO: Why is this joined?
-    const findChar = props.displayChar[0]
-        .replace("Arrow", "")
-        .replace("Return (ENTER)", "Enter");
 
     const foundChord
         = Array.from(allChords)
@@ -19,12 +16,16 @@ export function ChordDisplay(
                     .replace('&#x2581;', ' ')
                     .replace('(underscore)', '_')
                     // .replace('Return (ENTER)', '\r')
-                    == findChar;
+                    == props.displayChar[0];
             });
+
+    const foundChar = foundChord?.key
+        .replace("Arrow", "")
+        .replace("Return (ENTER)", "Enter");
     return (
         <div className="chord-image-holder" id="chord-image-holder" data-source="ErrorDisplay.tsx">
             <div className="col-sm-2 row generated next" id="chord2" >
-                <span id="char15" className="char">{findChar}</span>
+                <span id="char15" className="char">{foundChar}</span>
                 <img loading="lazy" alt="2" src={`${import.meta.env.BASE_URL}images/svgs/${foundChord?.chordCode}.svg`} width="75" className="hand"></img>
             </div>
         </div>
