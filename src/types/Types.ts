1a6afd96a5ce6c094bbdcc5e51e0f4d3027b76e4
Mark Stouffer <1802850+InTEGr8or@users.noreply.github.com>
2024-08-07 16:21:35 -0700

    Add 'g' and 'h' and '0' to achievements

--- src/types/Types.ts
+++ src/types/Types.ts
@@ -55,9 +55,9 @@ export type Achievement = {
 export const Achievements: Achievement[] = [
   { prompt: 'The most important key is the Return (ENTER) key. Press the thumb tip and release. You\'ll use this key to enter every command.\n\nNOTE: Type `tut` to run this tutorial again.', phrase: ['Return (ENTER)'], unlocked: false },
   // { prompt: 'The second most important key is the Backspace key. Just pull back the index finger.', phrase: ['DELETE (Backspace)'], command: 'x', unlocked: false },
-  { prompt: 'Type `fdsa` & Enter. Notice that it requires finger-pinch only.', phrase: 'fdsa'.split(''), unlocked: false },
-  { prompt: 'Type `jkl;`. Notice that it requires finger-grasp only.', phrase: 'jkl;'.split(''), unlocked: false },
-  { prompt: 'Press the thumb tip followed by a finger tip to type numbers 1-4', phrase: '1234'.split(''), unlocked: false },
+  { prompt: 'Type `gfdsa` & Enter. Notice that it requires finger-pinch only.', phrase: 'gfdsa'.split(''), unlocked: false },
+  { prompt: 'Type `hjkl;`. Notice that it requires finger-grasp only.', phrase: 'hjkl;'.split(''), unlocked: false },
+  { prompt: 'Press the thumb tip followed by a finger tip to type numbers 1-4', phrase: '01234'.split(''), unlocked: false },
   { prompt: 'Many characters require combinations followed by releasing all keys. Type `zxcv` and we\'ll show corrections as you type.', phrase: 'zxcv'.split(''), unlocked: false },
   { prompt: 'Type `play` to play a guided typing game with chord-hints.', phrase: 'play'.split(''), unlocked: false },
 ]
