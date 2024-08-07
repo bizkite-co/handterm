87f0858eaa2e338d5c1fa5461c1317927ef90615
Mark Stouffer <1802850+InTEGr8or@users.noreply.github.com>
2024-08-07 10:53:31 -0700

    WIP await import monaco-vim

--- src/components/MonacoEditor.tsx
+++ src/components/MonacoEditor.tsx
@@ -1,8 +1,6 @@
 // src/components/MonacoEditor.tsx
-
-import { useEffect, useRef, forwardRef, useImperativeHandle } from 'react';
+import React, { useEffect, useRef, forwardRef, useImperativeHandle } from 'react';
 import * as monaco from 'monaco-editor';
-import { initVimMode } from 'monaco-vim';
 import './MonacoEditor.css'; // Import the CSS file
 
 interface MonacoEditorProps {
@@ -13,92 +11,137 @@ interface MonacoEditorProps {
   height?: string;
 }
 
-const MonacoEditor = forwardRef<any, MonacoEditorProps>(({ initialValue, language, onChange, onSave, height = '400px' }, ref) => {
-  const editorRef = useRef<HTMLDivElement>(null);
-  const monacoEditorRef = useRef<monaco.editor.IStandaloneCodeEditor | null>(null);
+// Custom type for the imperative handle
+interface MonacoEditorHandle {
+  focus: () => void;
+  getValue: () => string;
+}
 
-  useImperativeHandle(ref, () => ({
-    focus: () => {
-      monacoEditorRef.current?.focus();
-    },
-    getValue: () => {
-      return monacoEditorRef.current?.getValue();
-    },
-  }));
+const MonacoEditor = forwardRef<MonacoEditorHandle, MonacoEditorProps>(
+  ({ initialValue, language, onChange, onSave, height = "90vh" }, ref) => {
+    const editorRef = useRef<HTMLDivElement>(null);
+    const monacoEditorRef = useRef<monaco.editor.IStandaloneCodeEditor | null>(null);
+    const vimModeRef = useRef<any>(null);
 
-  useEffect(() => {
-    if (editorRef.current) {
-      const editor = monaco.editor.create(editorRef.current, {
-        value: initialValue,
-        language: language,
-        theme: 'vs-dark',
-        automaticLayout: true,
-      });
+    useImperativeHandle(ref, () => ({
+      focus: () => {
+        monacoEditorRef.current?.focus();
+      },
+      getValue: () => {
+        return monacoEditorRef.current?.getValue() || ''; // Provide a default value
+      },
+    }));
 
-      monacoEditorRef.current = editor;
-
-      const statusBarContainer = document.createElement('div');
-      const vimMode = initVimMode(editor, statusBarContainer);
-
-      editor.onDidChangeModelContent(() => {
-        if (onChange) {
-          onChange(editor.getValue());
-        }
-      });
-
-      // Function to define Vim commands
-      const defineVimCommands = () => {
-        const Vim = (window as any).Vim;
-        if (Vim) {
-          Vim.defineEx('w', '', () => {
-            if (onSave) {
-              onSave(editor.getValue());
-            }
-          });
-
-          Vim.defineEx('q', '', () => {
-            if (editorRef.current) {
-              editorRef.current.style.display = 'none'; // Hide the editor
-            }
-          });
-
-          Vim.defineEx('wq', '', () => {
-            if (onSave) {
-              onSave(editor.getValue());
-            }
-            if (editorRef.current) {
-              editorRef.current.style.display = 'none'; // Hide the editor
-            }
-          });
-        } else {
-          console.error('Vim object is not available on the window');
+    useEffect(() => {
+      // Define MonacoEnvironment to load workers
+      (window as any).MonacoEnvironment = {
+        getWorkerUrl: function (_moduleId: string, label: string) {
+          if (label === 'json') {
+            return '/json.worker.bundle.js';
+          }
+          if (label === 'css') {
+            return '/css.worker.bundle.js';
+          }
+          if (label === 'html') {
+            return '/html.worker.bundle.js';
+          }
+          if (label === 'typescript' || label === 'javascript') {
+            return '/ts.worker.bundle.js';
+          }
+          return '/editor.worker.bundle.js';
         }
       };
 
-      // Ensure Vim commands are defined after Vim mode is initialized
-      // Retry every 100ms until Vim is available, up to a maximum of 10 retries
-      let retries = 0;
-      const maxRetries = 10;
-      const intervalId = setInterval(() => {
-        if ((window as any).Vim || retries >= maxRetries) {
-          clearInterval(intervalId);
-          if ((window as any).Vim) {
-            defineVimCommands();
-          } else {
-            console.error('Vim object was not available after maximum retries');
+      const loadMonacoEditor = async () => {
+        const { initVimMode } = await import('monaco-vim');
+
+        if (editorRef.current) {
+          const editor = monaco.editor.create(editorRef.current, {
+            value: initialValue,
+            language: language,
+            theme: 'vs-dark',
+            automaticLayout: true,
+          });
+
+          monacoEditorRef.current = editor;
+
+          const statusBarContainer = document.createElement('div');
+          statusBarContainer.className = 'status-node';
+          document.body.appendChild(statusBarContainer);
+
+          if (!vimModeRef.current) {
+            vimModeRef.current = initVimMode(editor, statusBarContainer);
           }
+
+          editor.onDidChangeModelContent(() => {
+            if (onChange) {
+              onChange(editor.getValue());
+            }
+          });
+
+          const defineVimCommands = async () => {
+            await new Promise<void>((resolve) => {
+              const checkVim = () => {
+                if ((window as any).Vim) {
+                  resolve();
+                } else {
+                  setTimeout(checkVim, 100);
+                }
+              };
+              checkVim();
+            });
+
+            const Vim = (window as any).Vim;
+            if (Vim) {
+              Vim.defineEx('w', '', () => {
+                if (onSave) {
+                  onSave(editor.getValue());
+                }
+              });
+
+              Vim.defineEx('q', '', () => {
+                if (editorRef.current) {
+                  editorRef.current.style.display = 'none'; // Hide the editor
+                }
+              });
+
+              Vim.defineEx('wq', '', () => {
+                if (onSave) {
+                  onSave(editor.getValue());
+                }
+                if (editorRef.current) {
+                  editorRef.current.style.display = 'none'; // Hide the editor
+                }
+              });
+            } else {
+              console.error('Vim object is not available on the window');
+            }
+          };
+
+          // Define Vim commands after ensuring Vim object is available
+          defineVimCommands();
+
+          return () => {
+            if (vimModeRef.current) vimModeRef.current.dispose();
+            editor.dispose();
+            if (statusBarContainer && statusBarContainer.parentNode) {
+              statusBarContainer.parentNode.removeChild(statusBarContainer);
+            }
+          };
         }
-        retries++;
-      }, 100);
+      };
+
+      loadMonacoEditor();
 
       return () => {
-        vimMode.dispose();
-        editor.dispose();
+        // Cleanup function
+        if (vimModeRef.current) vimModeRef.current.dispose();
+        if (monacoEditorRef.current) monacoEditorRef.current.dispose();
       };
-    }
-  }, [initialValue, language, onChange, onSave]);
+    }, [initialValue, language, onChange, onSave]);
 
-  return <div ref={editorRef} className="monaco-editor-container" style={{ height }} />;
-});
+    return <div ref={editorRef} className="monaco-editor-container" style={{ height }} />;
+  }
+);
 
 export default MonacoEditor;
