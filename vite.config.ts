c4d7738b635467568a1818239900a2d317e93541
Mark Stouffer <1802850+InTEGr8or@users.noreply.github.com>
2024-08-07 12:06:09 -0700

    Reduce vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,36 +1,12 @@
-// vite.config.ts
-
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import path from 'path'
-import monacoEditorPlugin from 'vite-plugin-monaco-editor'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import monacoEditorPlugin from 'vite-plugin-monaco-editor';
 
 export default defineConfig({
   plugins: [
     react(),
     monacoEditorPlugin({
-      languageWorkers: ['typescript', 'json', 'html', 'css'], // Use languageWorkers property
+      languageWorkers: ['typescript', 'json', 'html', 'css'],
     }),
   ],
-  base: '/',
-  build: {
-    target: 'esnext',
-    rollupOptions: {
-      output: {
-        manualChunks: {
-          jsonWorker: ['monaco-editor/esm/vs/language/json/json.worker'],
-          cssWorker: ['monaco-editor/esm/vs/language/css/css.worker'],
-          htmlWorker: ['monaco-editor/esm/vs/language/html/html.worker'],
-          tsWorker: ['monaco-editor/esm/vs/language/typescript/ts.worker'],
-          editorWorker: ['monaco-editor/esm/vs/editor/editor.worker'],
-        },
-      },
-    },
-  },
-  resolve: {
-    alias: {
-      '/shared/': path.resolve(__dirname, '../shared/'),
-      'monaco-editor': path.resolve(__dirname, 'node_modules/monaco-editor'),
-    },
-  },
-})
+});
