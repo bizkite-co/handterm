18e289cfd63243af78d8ca215ce227f4d0381d14
Mark Stouffer <1802850+InTEGr8or@users.noreply.github.com>
2024-08-12 08:59:41 -0700

    refactor: simplify vite configuration and remove unused monaco-editor alias

--- vite.config.ts
+++ vite.config.ts
@@ -12,21 +12,10 @@ export default defineConfig({
   build: {
     outDir: 'dist', // Output directory for build files
     target: 'esnext',
-    rollupOptions: {
-      output: {
-        manualChunks: {
-          jsonWorker: ['monaco-editor/esm/vs/language/json/json.worker'],
-          cssWorker: ['monaco-editor/esm/vs/language/css/css.worker'],
-          htmlWorker: ['monaco-editor/esm/vs/language/html/html.worker'],
-          tsWorker: ['monaco-editor/esm/vs/language/typescript/ts.worker'],
-          editorWorker: ['monaco-editor/esm/vs/editor/editor.worker'],
-        },
-      },
-    },
   },
   resolve: {
     alias: {
-      'monaco-editor': path.resolve(__dirname, 'node_modules/monaco-editor'),
+      // Remove monaco-editor alias
     },
   },
   worker: {
