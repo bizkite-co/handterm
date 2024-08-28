4af875b88bbca117eeb50b7982f6669b6568820b
Mark Stouffer <1802850+InTEGr8or@users.noreply.github.com>
2024-08-28 14:17:13 -0700

    fix: Update Prompt component layout and styling

--- src/components/Prompt.tsx
+++ src/components/Prompt.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { TerminalCssClasses } from "src/types/TerminalTypes"
+import { TerminalCssClasses } from "../types/TerminalTypes"
 
 interface PromptProps {
     domain: string;
@@ -8,10 +8,21 @@ interface PromptProps {
     githubUsername: string | null;
 }
 
+const githubLogo = '\uf09b';
+
 export const Prompt: React.FC<PromptProps> = ({ domain, username, timestamp, githubUsername }) => {
     return (
         <pre id={TerminalCssClasses.Prompt}>
-            <span className="domain">{domain}</span> <span className="user">{username}{githubUsername ? `@${githubUsername}` : ''}</span> <span className="timestamp">{timestamp}</span>
+            <span className="timestamp">{timestamp}</span>
+            <span className="user">{username}</span>
+            <span className="domain">@{domain}</span>
+            {githubUsername &&
+                <span className="github">
+                    <span className="github-logo">
+                        <i className="fa fa-github" aria-hidden="true"></i>
+                    </span> {githubUsername}
+                </span>
+            }
         </pre>
     )
 }
