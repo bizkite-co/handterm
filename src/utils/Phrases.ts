b69f831117281c9aaf925ad1f9445c2b6bac947c
alexstouffer <bizkite@hotmail.com>
2024-09-22 16:57:23 -0700

    tutorial steps complete: home-row keys

--- src/utils/Phrases.ts
+++ src/utils/Phrases.ts
@@ -11,12 +11,11 @@ const standardChars = /^[a-zA-Z0-9\s'";:.!,?]+$/;
 export default class Phrases {
     public static readonly phrases: PhraseType[] = [
         { key: "first-eight", value: "all sad lads ask dad; alas fads fall", tutorialGroup: "first-eight"},
-        { key: "nums", value: "0123 4567 8901 2345 6789 0987", tutorialGroup: "nums"},
-        { key: "ask", value: "Ask dad; A sad lass had salad." },
-        { key: "sad", value: "All sad lads fall." },
+        { key: "numbers", value: "0123 4567 8901 2345 6789 0987", tutorialGroup: "numbers"},
+        { key: "ask", value: "All lads had flasks as glad gals ask halls; all had a glass", tutorialGroup: "home-row" },
         { key: "gallant", value: "A gallant lad; a glass." },
         { key: "alas", value: "Alas, Khal's flask has a crack." },
-        { key: "lads", value: "Lads' flags fall as gaffs sag.", tutorialGroup: "home-row" },
+        { key: "lads", value: "Lads' flags fall as gaffs sag."},
         { key: "hello", value: "Hello, World!" },
         { key: "pack", value: "Pack my box with five dozen liquor jugs." },
         { key: "sphinx", value: "Sphinx of black quartz, judge my vow." },
