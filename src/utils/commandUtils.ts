b721110634628eac6477a8b6c8c161d2c9df8713
Mark Stouffer <1802850+InTEGr8or@users.noreply.github.com>
2024-11-24 19:10:11 -0800

    Check undefined args before joining

--- src/utils/commandUtils.ts
+++ src/utils/commandUtils.ts
@@ -12,7 +12,7 @@ export const saveCommandHistory = (commandHistory: any) => {
   localStorage.setItem(LogKeys.CommandHistory, JSON.stringify(commandHistory));
 }
 export function parsedCommandToString(cmd: ParsedCommand): string {
-  const argsStr = cmd.args.join(' ');
+  const argsStr = cmd.args ? cmd.args.join(' ') : '';
   const switchesStr = Object.entries(cmd.switches)
     .map(([key, value]) => {
       if (typeof value === 'boolean') {
