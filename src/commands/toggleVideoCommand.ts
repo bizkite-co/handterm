dad0484dea2fdb8a695002b79a5ffcff861d1a4a
Mark Stouffer <1802850+InTEGr8or@users.noreply.github.com>
2024-12-06 09:49:28 -0800

    fix: toggleVideo ICommandResponse

--- src/commands/toggleVideoCommand.ts
+++ src/commands/toggleVideoCommand.ts
@@ -1,4 +1,4 @@
-import { ICommand, ICommandContext } from '../contexts/CommandContext';
+import { ICommand, ICommandContext, ICommandResponse } from '../contexts/CommandContext';
 import { ParsedCommand } from '../types/Types';
 import { isShowVideoSignal } from '../signals/appSignals';
 import { ReactNode } from 'react';
@@ -6,17 +6,13 @@ import { ReactNode } from 'react';
 export const toggleVideoCommand: ICommand = {
     name: 'video',
     description: 'Toggle webcam video on/off',
-    execute: async (context: ICommandContext, parsedCommand: ParsedCommand) => {
+    execute: async (
+        context: ICommandContext, parsedCommand: ParsedCommand
+    ): Promise<ICommandResponse> => {
         isShowVideoSignal.value = !isShowVideoSignal.value;
-        context.appendToOutput({
-            response: `Video ${isShowVideoSignal.value ? 'enabled' : 'disabled'}` as ReactNode,
-            status: 200,
-            command: parsedCommand,
-            commandTime: new Date(),
-        });
         return {
-            status: 0,
-            message: 'Success',
+            status: 200,
+            message: `Video ${isShowVideoSignal.value ? 'enabled' : 'disabled'}`,
         };
     }
 };
