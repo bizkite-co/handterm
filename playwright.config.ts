83912de9ab6c27def3256a15cc342201d12456c2
Mark Stouffer <1802850+InTEGr8or@users.noreply.github.com>
2025-02-12 08:43:56 -0800

    Improve Playwright debugging

--- playwright.config.ts
+++ playwright.config.ts
@@ -105,7 +105,7 @@ export default defineConfig({
       name: 'chromium',
       use: {
         ...devices['Desktop Chrome'],
-        headless: true,
+        headless: !(process.env.DEBUG_PW ?? ''),
       },
     },
   ],
